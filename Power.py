53ac7f6c58d4a5b353fdfa517047635c9fb44751
Alex <mckcieply@gmail.com>
2022-01-31 12:54:26 +0100

    creating tournament script

---
+++ Power.py
@@ -0,0 +1,12 @@
+def num_rounds():
+
+    basic = 2 
+    k = int(input ("Number of rounds in tournament: "))
+    rounds = 1
+    while k > 0 :
+        rounds  *= basic
+        k -= 1
+
+
+def divide():
+    
