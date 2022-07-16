80f2e1dab5b6b77345a27cad56fb1447a4c3b537
Alex <mwppl123@gmail.com>
2022-07-16 19:10:56 +0200

    Splittin' AMD 'n' Intel + fixin' format

--- price_range.py
+++ price_range.py
@@ -2,16 +2,35 @@
 
 from bs4 import BeautifulSoup
 import requests
-
-url = "https://www.komputronik.pl/category/401/procesory.html"
+inp = input("Do you want to search for a AMD or Intel CPUS? ")
+if (inp.lower() == "amd"):
+    cpu = "amd"
+elif (inp.lower() == "intel"):
+    cpu = "intel"
+else:
+    print("You gave wrong value")
+    
+url = f"https://www.komputronik.pl/search-filter/401/procesory-{cpu}"
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 ul = doc.find("ul", class_ = "product-entry2-wrap")
 for li in ul.find_all("li", class_ = "product-entry2"):
     cena = list(li.find("span", class_ = "price").contents[1].strings)
     nazwa = li.find("div", class_= "inline-features").string
+    proc = li.find("div", class_ = "pe2-head").contents[1].string.strip()
+    if (proc == "POLECANE"): proc = li.find("div", class_ = "pe2-head").contents[3].string.strip()
     podzial = nazwa.strip().split("|")
-    wynik = f"{podzial[0]}/{podzial[1]}/{podzial[2]} {cena[0].strip()} {cena[1]}"
+    details = list(li.find("ul", class_ = "key-features2 pe2-desc").contents)
+    cores = details[7].contents[1].string.strip().split(" ")
+    mhz = details[9].contents[1].string.strip()
+    tmhz = details[11].contents[1].string.strip()
+
+    wynik = f"""{proc} 
+{cores[0]} Cores | {mhz} - {tmhz}
+{cena[0].strip()} {cena[1]}
+"""
+        
+    
     print(wynik)
 
     
