9913a2d2b7b4f7d9d561f83a305536f21fcff28a
Alex <mwppl123@gmail.com>
2022-07-18 23:00:38 +0200

    UI change

--- OOP1.py
+++ OOP1.py
@@ -1,25 +1,29 @@
 #Basic banking system
 
 class User():
-    def __init__(self, name, age, gender):
-        self.name = name
+    def __init__(self, fname, lname, age, gender):
+        self.fname = fname
+        self.lname = lname
         self.age = age
         self.gender = gender
     def details(self):
         print(f"""Personal Data:
-Name: {self.name}
+firstname: {self.fname}
+lastname: {self.lname}
 Age: {self.age}
 Gender: {self.gender} 
 """)
 
 class Bank(User):
-    def __init__(self,name,age, gender):
-        super().__init__(name,age,gender)
+    def __init__(self,fname,lname, age, gender):
+        super().__init__(fname,lname, age,gender)
         self.balance = 0
+
     def deposit(self, amount): 
         self.balance += amount
         print(f"Balance has ben updated: ${self.balance}")
-    def withdraw(self,amount):
+
+    def withdraw(self, amount):
         self.amount = amount
         if(self.amount > self.balance ):
             print(f"Insufficient funds. Balance avalible: ${self.balance}")
@@ -31,10 +35,18 @@ class Bank(User):
         self.details()
         print(f"Account balance: ${self.balance}")
 
-u1= Bank("Alex", 20, "Male")
+d1 = input("""
+What would you like to do?
+a) Create an Account.
+b) Log In
+""")
 
-u1.deposit(200)
+if d1 in ["A", "a"]:
+    d2_fname = input("Please input your first name: ")   
+    d2_lname = input("Please input your last name: ")
+    d2_age = input("Please input your age: " )
+    d2_gender = input("Please input your gener(Male/Female/Neither): ")
+    p1 = User(d2_fname, d2_lname, d2_age, d2_gender)
 
-u1.withdraw(50)
+    
 
-u1.view_balance()
