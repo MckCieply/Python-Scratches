24b4ae4a92bd08fc259cd5f08cf44f11dac98bfd
Alex <mwppl123@gmail.com>
2022-07-19 14:03:19 +0200

    Loggin + hashin made

--- OOP1.py
+++ OOP1.py
@@ -1,13 +1,27 @@
 #Basic banking system
 
+
+
 class User():
-    def __init__(self, fname, lname, age, gender):
+    def __init__(self, fname, lname, age, gender, login, password):
         self.fname = fname
         self.lname = lname
         self.age = age
         self.gender = gender
+        self.login = login 
+        self.password = password
+
+    def password_hash(self):
+        lenght = len(self.password)
+        hash = ""
+        for i in range(lenght):
+            hash += "*"
+        return hash
+
     def details(self):
         print(f"""Personal Data:
+Login: {self.login}
+Password: {self.password_hash()}
 firstname: {self.fname}
 lastname: {self.lname}
 Age: {self.age}
@@ -46,7 +60,14 @@ if d1 in ["A", "a"]:
     d2_lname = input("Please input your last name: ")
     d2_age = input("Please input your age: " )
     d2_gender = input("Please input your gener(Male/Female/Neither): ")
+    d2_login = input("Please input login you want to use: ")
+    d2_password = input("Please input password of your choosing: ")
     p1 = User(d2_fname, d2_lname, d2_age, d2_gender)
 
-    
+elif d1 in ["B", "b"]:
+    d2_login = input("Please input your login: ")
+    d2_password = input("Please input your password: ")
 
+# u1 = User("Alex","Nonimprotant", 20, "Male", "MckCieply", "4lit")
+
+# u1.details()
