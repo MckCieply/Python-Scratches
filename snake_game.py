d19b36812335e44482140dbb3fe00a81c639b5ff
Alex <mwppl123@gmail.com>
2022-07-28 14:19:15 +0200

    Score and game over functions

--- snake_game.py
+++ snake_game.py
@@ -22,3 +22,26 @@ s_direction = 'RIGHT'
 change_to = s_direction
 f_position = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
 f_spawn = True
+
+score = 0
+def show_score(choice, color, font, size):
+    sc_font = pygame.font.SysFont(font, size)
+    sc_surface = sc_font.render(f"Score: {str(score)}", True, color)
+    sc_rect = sc_surface.get_rect()
+
+    window.blit(sc_surface, sc_rect)
+
+def game_over():
+    font = pygame.font.SysFont('times new roman', 50)
+    g_over_surface = font.render(f"Your score is: {str(score)}", True, red)
+    g_over_rect = g_over_surface.get_rect()
+    g_over_rect.midtop = (window_x/2, window_y/4)
+
+    window.blit(g_over_surface, g_over_rect)
+    pygame.display.flip()
+    time.sleep(2)
+    
+    pygame.quit()
+    quit()
+
+
