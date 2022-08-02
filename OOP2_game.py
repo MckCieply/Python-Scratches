ca25478a23a75abebdfe73741085579a02336fe1
Alex <mwppl123@gmail.com>
2022-08-02 13:59:35 +0200

    frames done

--- OOP2_game.py
+++ OOP2_game.py
@@ -1,40 +1,49 @@
 from tkinter import *
+import OOP2_settings as settings
+import OOP2_utils as utils
 
 root = Tk()
-root.configure(bg = "lightgray")
-root.geometry('1440x720')
+# root.configure(bg = "lightgray")
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("OOP2_MineSweeper")
 root.resizable(False, False)
 
 def framing():
-    top_frame = Frame(
+    top_frame  = Frame(
         root,
-        bg='grey',
-        width = '1440',
-        height = '144'
+        bg='gray',
+        width = utils.width_prct(60),
+        height = utils.height_prct(20)
     )
     left_frame = Frame(
         root,
         bg='grey',
-        width = '288',
-        height = '720'
+        width = utils.width_prct(20),
+        height = settings.HEIGHT
     )
     bottom_frame = Frame(
         root,
         bg='grey',
-        width = '1440',
-        height = '144'
+        width = utils.width_prct(60),
+        height = utils.height_prct(20)
     )
     right_frame = Frame(
         root,
         bg='grey',
-        width = '288',
-        height = '720'
+        width = utils.width_prct(20),
+        height = settings.HEIGHT
+    )
+    main_frame = Frame(
+        root,
+        bg='white',
+        width = utils.width_prct(60),
+        height = utils.height_prct(60)
     )
 
-    top_frame.place(x=0, y=0)
+    top_frame.place(x=288, y=0)
     left_frame.place(x=0, y=0)
     bottom_frame.place(x=288, y=576)
     right_frame.place(x=1152, y=0)
+    main_frame.place(x=utils.width_prct(20), y=utils.height_prct(20))
 framing()
 root.mainloop()
