ca0893ad93cd409222b6023f7850c1817785cf8f
Alex <mwppl123@gmail.com>
2022-10-30 16:25:50 +0100

    Re-name and input features

---
+++ advanced_calculator.py
@@ -0,0 +1,19 @@
+def input_numbers():
+    input = True
+    print("""
+          WELCOME IN ADVANCED CALCULATOR
+          PLEASE CHOOSE WHAT WOULD YOU LIKE TO CALCULATE:
+          1) MEDIAN
+          """)
+    choice = input("Pass a coresponding number: ")
+    if choice in 1:
+        print("You will calculate median now!")
+        while input != False:
+            number = input("Please input your numbers one at the time: ")
+            
+
+def calculate_median(numbers_list):
+    max, min = None
+    for number in number_list:
+        pass
+        
