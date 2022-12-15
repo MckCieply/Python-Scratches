42979f4b4343b18a57bc870da4ac829713da9524
Alex <mwppl123@gmail.com>
2022-12-15 12:30:55 +0100

    Skeleton

--- physicsBoxPlot.py
+++ physicsBoxPlot.py
@@ -1,3 +1,17 @@
 #i have constant data and gotta make plot of those
 #think box is the best
 import matplotlib as plt
+#Rod 1
+#w/o weights
+measur_11 = []
+#w wide weights
+measur_12 = []
+#w central weights
+measur_13 = []
+#Rod 2
+#w/o weights
+measur_21 = []
+#w wide weights
+measur_22 = []
+#w central weights
+measur_23 = []
