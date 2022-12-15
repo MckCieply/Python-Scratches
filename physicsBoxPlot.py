2cb83eb24dabd37e51941f77ec5615e57bec87ea
Alex <mwppl123@gmail.com>
2022-12-15 12:52:46 +0100

    Data insert

--- physicsBoxPlot.py
+++ physicsBoxPlot.py
@@ -1,17 +1,18 @@
 #i have constant data and gotta make plot of those
 #think box is the best
 import matplotlib as plt
+#2 periods of vibration each
 #Rod 1
 #w/o weights
-measur_11 = []
+measur_11 = [1.65, 1.57, 1.55, 1.65, 1.62]
 #w wide weights
-measur_12 = []
+measur_12 = [3.60, 3.68, 3.60, 3.69, 3.54]
 #w central weights
-measur_13 = []
+measur_13 = [1.84, 1.81, 1.85, 1.81, 1.78]
 #Rod 2
 #w/o weights
-measur_21 = []
+measur_21 = [1.20, 1.22, 1.22, 1.23, 1.32]
 #w wide weights
-measur_22 = []
+measur_22 = [2.68, 2.64, 2.50, 2.73, 2.79]
 #w central weights
-measur_23 = []
+measur_23 = [1.38, 1.34, 1.38, 1.34, 1.29]
