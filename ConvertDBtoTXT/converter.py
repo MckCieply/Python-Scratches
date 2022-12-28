6633d019069bbabc7eed6a15d9fc3654fad31b2d
Alex <mwppl123@gmail.com>
2022-12-28 15:24:46 +0100

    Writing query contents to txt file

--- ConvertDBtoTXT/converter.py
+++ ConvertDBtoTXT/converter.py
@@ -3,3 +3,7 @@ conn = sql.connect("convertDBtoTXT\deals.db")
 cur = conn.cursor()
 query = cur.execute("SELECT id, brand, model, price, year, mileage FROM cars")
 
+with open("convertDBtoTXT\cars.txt","w") as f:
+    for el in query:
+        f.write(f"{el[0]},{el[1]},{el[2]},{el[3]},{el[4]},{el[5]}\n")
+        
