bc7ce987cdaeee8efc0e2b2997878faae9cd881b
Alex <mwppl123@gmail.com>
2023-03-21 15:53:57 +0100

    working distinct

--- distinct.py
+++ distinct.py
@@ -1,9 +1,7 @@
 def main():
     nums = [1,2,3,3]
-    for i, num in enumerate(nums):
-        print(nums)
-        nums.pop(i)
-        #if num in nums:
-        #    return True
-        
+    nums2 = list(set(nums))
+    if len(nums) != len(nums2):
+        return True
+            
 main()
