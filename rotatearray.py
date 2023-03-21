a85dd06d46cff4c4c9accada78720e9626a47801
Alex <mwppl123@gmail.com>
2023-03-21 10:13:35 +0100

    working algorithm

--- rotatearray.py
+++ rotatearray.py
@@ -1,9 +1,12 @@
 nums = [1,2,3,4,5,6,7]
 k = 2
-
 for i in range(k):
-    for j, num in enumerate(nums):
-        temp = nums[j+1]
-        nums[j+1] = nums[j]
-        
+    temp = nums[0]
+    nums[0] = nums[-1]
+    for j, num in enumerate(nums[1:]):
+        moving = nums[j+1]
+        nums[j+1] = temp
+        temp = moving
+            
+print(nums)
         
