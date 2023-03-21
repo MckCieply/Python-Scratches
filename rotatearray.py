fe59d71cd30a2fb5d2adb74a7210a3719af26b1d
Alex <mwppl123@gmail.com>
2023-03-21 10:14:20 +0100

    changing names

--- rotatearray.py
+++ rotatearray.py
@@ -1,12 +1,12 @@
 nums = [1,2,3,4,5,6,7]
 k = 2
 for i in range(k):
-    temp = nums[0]
+    moving = nums[0]
     nums[0] = nums[-1]
     for j, num in enumerate(nums[1:]):
-        moving = nums[j+1]
-        nums[j+1] = temp
-        temp = moving
+        temp = nums[j+1]
+        nums[j+1] = moving
+        moving = temp
             
 print(nums)
         
