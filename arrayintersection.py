b97d155bf4d3005505e04a7cabbb89c4c529ecf6
Alex <mwppl123@gmail.com>
2023-03-24 10:17:52 +0100

    Initial logic

---
+++ arrayintersection.py
@@ -0,0 +1,17 @@
+num1 = [1,2,3,4]
+num2 = [7,6,5,4,3]
+
+def arraysearch(num1, num2):
+    results = []
+    if(len(num1) < len(num2)):
+        for num in num1:
+            if num in num2:
+                if num not in results:
+                    results.append(num)
+    else:
+        for num in num2:
+            if num in num1:
+                if num not in results:
+                    results.append(num)
+    
+    return results
